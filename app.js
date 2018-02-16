8d734903bdc7d0c7a8a24712267f406ad2d92a4b
jspiders <jspiders@gmail.com>
2018-02-16 09:52:22 +0530

    testapp

--- app.js
+++ app.js
@@ -1,23 +1,107 @@
-var myapp = angular.module("testApp" , ["ngRoute"]);
+var myapp = angular.module("app" , ["ngRoute"]);
 
 
-// route
-myapp.config(function($routeProvider , $locationProvider){
+
+myapp.controller("testCtr" , function($scope , $timeout ,$interval){
+$scope.message = "hello angularjs";
+$timeout(function(){
+    $scope.message = "hello nodejs";
+},10000);
+
+
+
+$scope.time = new Date().toLocaleTimeString();
+$interval(function(){
+    $scope.time = new Date().toLocaleTimeString();
+},1000)
+
+});
+
+
+
+
+
+myapp.config(["$routeProvider" , "$locationProvider" , function($routeProvider , $locationProvider ){
     $routeProvider.when("/login" , {
-        templateUrl:"/views/login.html"
+        templateUrl : "/views/login.html"
     }).when("/register" , {
-        templateUrl:"/views/register.html"
+        templateUrl : "/views/register.html"
     }).otherwise({
         redirectTo:"/login"
     });
-
-})
-
+}]);
 
 
 
+myapp.filter("shashi" , function(){
+    return function(input , limit){
+        if(isNaN(limit) || limit == " "){
+           return input;
+        }else {
+        return input.substring(0 , limit ).toUpperCase();
+        }
+    }
+});
 
-//controller
 myapp.controller("myCtr" , function($scope){
-    $scope.test
-})
+    $scope.users = [{
+        firstname : "vinay",
+        lastName : "kumar",
+        Number : 20,
+        company:"qspiders",
+        salary:30000,
+        designation:"web developer",
+        dob : new Date('05-10/1990')
+    },
+    {
+        firstname : "arun",
+        lastName : "kumar",
+        Number : 21,
+        company:"jspiders",
+        salary :20000,
+        designation:"java developer",
+        dob : new Date('05-10/1980')
+    },
+    {
+        firstname : "manu",
+        lastName : "kumar",
+        Number : 25,
+        company:"testyantra",
+        salary :50000,
+        designation:"node developer",
+        dob : new Date('05-10/1970')
+    },
+    {
+        firstname : "vinay",
+        lastName : "kumar",
+        Number : 20,
+        company:"qspiders",
+        salary:30000,
+        designation:"web developer",
+        dob : new Date('05-10/1990')
+    },
+    {
+        firstname : "arun",
+        lastName : "kumar",
+        Number : 21,
+        company:"jspiders",
+        salary :20000,
+        designation:"java developer",
+        dob : new Date('05-10/1980')
+    },
+    {
+        firstname : "manu",
+        lastName : "kumar",
+        Number : 25,
+        company:"testyantra",
+        salary :50000,
+        designation:"node developer",
+        dob : new Date('05-10/1970')
+    }
+
+]
+});
+
+
+
+
